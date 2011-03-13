457d41c68904c0bb900e2b9aa5efaf1a70ac2c03
Brent Theisen <brent@designspiders.com>
2011-03-13 00:11:10 -0600

    Refactored some JS stuff, renamed views to .html.erb and started work on the default follower query.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ActiveRecord::Base
   belongs_to :twitter_user
   has_many :follows
   has_many :followed_twitter_users, { :through => :follows, :source => :twitter_user, :class_name => 'TwitterUser' }
+  
+  def default_search_twitter_users
+    
+    # Need to come back to this code
+    
+#    TwitterUser.
+#      joins(
+#        [
+#          'join follows on (follows.twitter_user_id=twitter_user.id)',
+#          'left join users on (users.twitter_user_id=twitter_users.id)',
+#        ]
+#      ). 
+#      conditions (['follows.user_id', self.id])
+#      
+#      :conditions => [],
+#      :group
+#    )
+  end
     
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+	
+    $('form.ajax').submit(function(e) {
+        e.preventDefault();
+        
+        $.ajax({ url: $(this).attr('action'), type: $(this).attr('method'), data: $(this).serialize(), dataType: 'script' });
+    });
+    
+    $('a.ajax').click(function(e) {
+        e. preventDefault();
+        
+        $.ajax({ url: $(this).attr('href'), dataType: 'script'});
+    });
+});
