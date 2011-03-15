c41d0681cb6050c66a0adb31ce595ad0b9594045
Brent Theisen <brent@designspiders.com>
2011-03-14 20:25:19 -0500

    Mucking with the queries that populate home#index.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
       return
     end
 
-    @past_bounces = Bounce.where(['hide_past_bounces=? and executed_at is not null', false])
-    @follows = Follow.where(['user_id=?', @logged_in_user.id]).includes([:twitter_user, :bounce])
+    @past_bounces = Bounce.where(['hide_past_bounces=? and executed_at is not null', false]).order('executed_at desc').limit(3)
+    @follows = Follow.where(['user_id=?', @logged_in_user.id]).includes([:twitter_user, :bounce]).limit(20)
   end
 end
