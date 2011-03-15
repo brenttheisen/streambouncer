c8213f7a1ebd3a65292e8aa15ca558eabfcee360
Brent Theisen <brent@designspiders.com>
2011-03-14 22:04:46 -0500

    Changed default and regular queries. Auto complete is starting to work.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,6 @@ class HomeController < ApplicationController
     end
 
     @past_bounces = Bounce.where(['hide_past_bounces=? and executed_at is not null', false]).order('executed_at desc').limit(3)
-    @follows = Follow.where(['user_id=?', @logged_in_user.id]).includes([:twitter_user, :bounce]).limit(20)
+    @follows = Follow.joins('left join bounces on (follows.bounce_id=bounces.id)').where(['follows.user_id=?', @logged_in_user.id]).order('if(follows.bounce_id is null, 1, 0), bounces.take_action_at').offset(params[:o]).limit(20)
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,9 @@ class SearchController < ApplicationController
     @remove_results = params[:o].nil? || params[:o] == 0
     if params[:q].nil? || params[:q].strip.length == 0
       # Default query
-      @follows = Follow.joins('left join bounces on (follows.bounce_id=bounces.id)').order('if(follows.bounce_id is null, 1, 0), bounces.take_action_at').offset(params[:o]).limit(20)
+      @follows = Follow.joins('left join bounces on (follows.bounce_id=bounces.id)').where(['follows.user_id=?', @logged_in_user.id]).order('if(follows.bounce_id is null, 1, 0), bounces.take_action_at').offset(params[:o]).limit(20)
     else
-      @follows = Follow.where(['twitter_users.username like ?', "%#{params[:q]}%"]).offset(params[:o]).limit(20)
+      @follows = Follow.joins('join twitter_users on (follows.twitter_user_id=twitter_users.id)').where(['follows.user_id=? and twitter_users.username like ?', @logged_in_user.id, "%#{params[:q]}%"]).offset(params[:o]).limit(20)
     end
   end
 end
