327b0e7a8619e5fb9c895ecae866955a80c6ea0b
Brent Theisen <brent@designspiders.com>
2011-03-15 23:16:53 -0500

    Better logging.

--- app/controllers/unbounce_controller.rb
+++ app/controllers/unbounce_controller.rb
@@ -2,10 +2,12 @@ class UnbounceController < ApplicationController
   def index
     @follow = Follow.where([ 'id=? and user_id=?', params[:id], @logged_in_user.id]).first
     
-    @bounce = @follow.bounce
-    @bounce.canceled_at Time.now
-    @bounce.active = false
-    @bounce.save
+    bounce = @follow.bounce
+    if bounce.nil?
+      bounce.canceled_at Time.now
+      bounce.active = false
+      bounce.save
+    end
     
     @follow.bounce = nil
     @follow.save
