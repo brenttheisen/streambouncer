6b83f08c4df6155a9e80b2614b5893bd7c96ec06
Brent Theisen <brent@designspiders.com>
2011-03-16 00:11:31 -0500

    Forgot to remove array.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   APP_DOMAIN = 'streambouncer.com'
 
   def ensure_domain
-    if [::Rails.env] == 'production' && request.env['HTTP_HOST'] != APP_DOMAIN
+    if ::Rails.env == 'production' && request.env['HTTP_HOST'] != APP_DOMAIN
       # HTTP 301 is a "permanent" redirect
       redirect_to "http://#{APP_DOMAIN}", :status => 301
     end
