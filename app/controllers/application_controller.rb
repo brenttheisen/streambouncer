42c28c9ab0cc5710b2ebd236f874fb1ca586f4fc
Brent Theisen <brent@designspiders.com>
2011-03-16 00:08:36 -0500

    Redirect to normalized domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  before_filter :ensure_domain
   before_filter :set_user_controller
   after_filter :set_user_session
+
+  APP_DOMAIN = 'streambouncer.com'
+
+  def ensure_domain
+    if [::Rails.env] == 'production' && request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
   
   def host_port_name
     self.request.port == 80 ? self.request.host : "#{self.request.host}:#{self.request.port}"
