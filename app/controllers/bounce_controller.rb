26f2dc4f0d2bae96bce4bfa7c21308245bc81590
Brent Theisen <brent@designspiders.com>
2011-03-15 22:46:54 -0500

    Real bounce implementation. People get unfollowed and a job is setup to re-follow them.

--- app/controllers/bounce_controller.rb
+++ app/controllers/bounce_controller.rb
@@ -4,9 +4,19 @@ class BounceController < ApplicationController
     @bounce = @follow.bounce
     @bounce ||= Bounce.new
     
-    # Take action on the bounce in 5 minutes
-    @bounce.take_action_at = Time.at(params[:t].to_i)
+    client = TwitterOAuth::Client.new(
+      :consumer_key => StreamBouncer::Application.config['twitter_consumer_key'],
+      :consumer_secret => StreamBouncer::Application.config['twitter_consumer_secret'],
+      :token => @logged_in_user.twitter_access_token, 
+      :secret => @logged_in_user.twitter_access_token_secret
+    )
+    client.unfriend(@follow.twitter_user.twitter_id)
+    
+    @bounce.take_action_at = 5.minutes.from_now
+    # @bounce.take_action_at = Time.at(params[:t].to_i)
     @bounce.save
+    
+    Delayed::Job.enqueue(@bounce, @bounce.take_action_at)
 
     @follow.bounce = @bounce
     @follow.save
--- app/models/bounce.rb
+++ app/models/bounce.rb
@@ -1,3 +1,29 @@
 class Bounce < ActiveRecord::Base
   has_one :follow
+  
+  def perform
+    return unless self.active
+    
+    self.reload
+    
+    follow = self.follow
+    user = follow.user
+    client = TwitterOAuth::Client.new(
+      :consumer_key => StreamBouncer::Application.config['twitter_consumer_key'],
+      :consumer_secret => StreamBouncer::Application.config['twitter_consumer_secret'],
+      :token => user.twitter_access_token, 
+      :secret => user.twitter_access_token_secret
+    )
+    
+    twitter_user = follow.twitter_user
+    client.friend(twitter_user.twitter_id)
+    
+    follow.bounce_id = nil
+    follow.save 
+    
+    self.active = false
+    self.executed_at Time.now
+    self.follow = nil
+    self.save
+  end
 end
