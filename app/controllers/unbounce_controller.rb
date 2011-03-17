bdc504a40b7a2d7f46e1305ba84937309560d693
Brent Theisen <brent@designspiders.com>
2011-03-17 00:16:24 -0500

    Implemented cancel/cancel and follow now buttons.

--- app/controllers/unbounce_controller.rb
+++ app/controllers/unbounce_controller.rb
@@ -8,11 +8,15 @@ class UnbounceController < ApplicationController
       bounce.active = false
       bounce.save
     end
-    
-    logger.info "Refollowing #{@follow.twitter_user.username} (#{@follow.twitter_user.twitter_id}) for #{@logged_in_user.twitter_user.username} (#{@logged_in_user.twitter_user.twitter_id})"
-    twitter_client = @logged_in_user.twitter_client
-    response = twitter_client.friend(@follow.twitter_user.twitter_id)
-    logger.info "Refollow response... #{response.to_json}"
+
+    if params[:f].nil? || params[:f] == '1'
+      logger.info "Refollowing #{@follow.twitter_user.username} (#{@follow.twitter_user.twitter_id}) for #{@logged_in_user.twitter_user.username} (#{@logged_in_user.twitter_user.twitter_id})"
+      twitter_client = @logged_in_user.twitter_client
+      response = twitter_client.friend(@follow.twitter_user.twitter_id)
+      logger.info "Refollow response... #{response.to_json}"
+    else
+      logger.info "Canceling bounce without re-follow: #{@follow.twitter_user.username} (#{@follow.twitter_user.twitter_id}) for #{@logged_in_user.twitter_user.username} (#{@logged_in_user.twitter_user.twitter_id})"
+    end    
     
     @follow.bounce = nil
     @follow.save
