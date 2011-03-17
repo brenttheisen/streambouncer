539ac223684c0b3f5c079673cc8f422312453ff1
Brent Theisen <brent@designspiders.com>
2011-03-17 12:30:45 -0500

    Render 404 without layout and make sure logged_in_user isn't null.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
   ADMIN_TWITTER_USERNAMES = [ 'brenttheisen', 'danroose' ]
   
   def index
-    render :file => "#{RAILS_ROOT}/public/404.html" unless ADMIN_TWITTER_USERNAMES.include?(@logged_in_user.twitter_user.username)
+    render :layout => false, :file => "#{RAILS_ROOT}/public/404.html" if @logged_in_user.nil? || !ADMIN_TWITTER_USERNAMES.include?(@logged_in_user.twitter_user.username)
     
     @users = User.order('last_login_at desc').includes([ :twitter_user ])
   end
